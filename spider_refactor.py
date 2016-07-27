3a27f58ccfdb0c7b91aa6a9551a794fd17fca800
hanchang <hanchang_96@163.com>
2016-07-27 09:14:46 +0800

    maybe final version: set time-interval,set retry-mechanism, but no effect

--- spider_refactor.py
+++ spider_refactor.py
@@ -4,47 +4,79 @@
 import urllib,urllib2
 import cookielib
 import re
-
+import time
+import random
 
 class Login(object):
     def __init__(self, id, semester):
         print "Initialing : ..."
         self.stuid = str(id)
-        self.semester=semester
+        self.semester = semester
         self.url = 'http://xk.urp.seu.edu.cn/jw_service/service/stuCurriculum.action'
-
+        self.referer = 'http://xk.urp.seu.edu.cn/jw_service/service/lookCurriculum.action'
         self.cookies = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
+        self.opener.open(self.referer)
 
         self.data = {'queryStudentId':self.stuid,'queryAcademicYear':self.semester}
         self.post_data = urllib.urlencode(self.data)
-        self.request = urllib2.Request(self.url,self.post_data)
+        # output cookies for check
+        # print self.cookies
 
-        self.response = self.opener.open(self.request)
+        # self.header = {
+        #     'Host': 'xk.urp.seu.edu.cn',
+        #     'Connection': 'keep-alive',
+        #     'Content-Length':'60',
+        #     'Cache-Control': 'max-age=0',
+        #     'Origin': 'http://xk.urp.seu.edu.cn',
+        #     'Upgrade-Insecure-Requests': '1',
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36',
+        #     'Referer': 'http://xk.urp.seu.edu.cn/jw_service/service/lookCurriculum.action',
+        #     'Content-Type': 'application/x-www-form-urlencoded',
+        #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        #     'Accept-Encoding': 'gzip, deflate',
+        #     'Accept-Language': 'zh-CN,zh;q=0.8'
+        #     }
+        self.header = {
+            'Host': 'xk.urp.seu.edu.cn',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cache-Control': 'max-age=0',
+            'Origin': 'http://xk.urp.seu.edu.cn',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) Chrome/51.0.2704.106',
+            'Referer': 'http://xk.urp.seu.edu.cn/jw_service/service/lookCurriculum.action',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+            }
+        self.request = urllib2.Request(self.url,self.post_data,self.header)
+
+        # self.response = self.opener.open(self.request)
+        for j in range(5):
+            try:
+                print "The ", j+1, " times try"
+                self.response = self.opener.open(self.request)
+                for item in self.cookies:
+                    print item.name,' = ',item.value
+                break
+            except urllib2.URLError, e:
+                print "in the except block"
+                print e.code,e.reason
+                time.sleep(random.uniform(8,15))
+                print "wait for 10 seconds"
+                pass
         # try:
         #     self.response = self.opener.open(self.request)
         #     pass
-        # except urllib2.URLError,e:
-        #     if hasattr(e,"code"):
+        # except urllib2.URLError, e:
+        #     if hasattr(e, "code"):
         #         print e.code
-        #     if hasattr(e,"reason"):
+        #     if hasattr(e, "reason"):
         #         print e.reason
-        # except Exception,e:
-        #     print Exception,":",e
-
-        # else:
-        #     print "error when open(request)"
-
-        # for item in self.cookies:
-        #     print item.name,' = ',item.value
+        # except Exception, e:
+        #     print Exception, ":", e
 
     def get_info(self):
-        #post data
-        # data = {'queryStudentId':self.stuid,'queryAcademicYear':self.semester}
-        # post_data=urllib.urlencode(data)
-        # request = urllib2.Request(self.url,post_data)
-
-        # response = self.opener.open(self.request)
         page = self.response.read().decode('utf-8')
         pattern = re.compile(r'<td.*?width="20%".*?align="left">(.*?)</td>', re.S)
 
@@ -64,8 +96,9 @@ class Login(object):
             return "No data"
 
 if __name__ == '__main__':
-    for i in range(14113101,14113499):
+    for i in range(213140001,213143999):
         semester='16-17-1'
-        log=Login(i,semester)
+        log = Login(i,semester)
         log.get_info()
         del log
+        time.sleep(random.uniform(20,25))
