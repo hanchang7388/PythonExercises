7448459460198454656fafbf46e095ec7372009c
hanchang <hanchang_96@163.com>
2016-08-16 17:14:47 +0800

    complete

--- restart_process.py
+++ restart_process.py
@@ -5,21 +5,26 @@ import time
 import sys
 import os
 
+num = 0
 
 def restart_program():
     # get compile path
     # like:  >>> sys.executable
     #         'C:\\Python27\\python.exe'
     python = sys.executable
-
+    print sys.argv
     # an os interface to replace the current process, pid dont get changed
     os.execl(python, python, * sys.argv)
+    print 'restart...pid:', os.getpid()
 
 if __name__ == "__main__":
-    print 'start...'
+    print 'start...pid:', os.getpid()
+    num += 1
+    print num
 #  answer = raw_input("Do you want to restart this program ? ")
 #  if answer.strip() in "y Y yes Yes YES".split():
 #    restart_program()
     print u"1秒后,程序将结束并重启..."#.encode("utf-8")
     time.sleep(1)
     restart_program()
+    # print 'restart...pid:', os.getpid()
--- spider_refactor.py
+++ spider_refactor.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib,urllib2
+import urllib, urllib2
 import cookielib
 import re
 import time
 import random
+import codecs
+
 
 class Login(object):
     def __init__(self, id, semester):
-        print "Initialing : ..."
+        # print "Initialing : ..."
         self.stuid = str(id)
         self.semester = semester
         self.url = 'http://xk.urp.seu.edu.cn/jw_service/service/stuCurriculum.action'
@@ -18,25 +20,9 @@ class Login(object):
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
         self.opener.open(self.referer)
 
-        self.data = {'queryStudentId':self.stuid,'queryAcademicYear':self.semester}
+        self.data = {'queryStudentId': self.stuid, 'queryAcademicYear': self.semester}
         self.post_data = urllib.urlencode(self.data)
-        # output cookies for check
-        # print self.cookies
-
-        # self.header = {
-        #     'Host': 'xk.urp.seu.edu.cn',
-        #     'Connection': 'keep-alive',
-        #     'Content-Length':'60',
-        #     'Cache-Control': 'max-age=0',
-        #     'Origin': 'http://xk.urp.seu.edu.cn',
-        #     'Upgrade-Insecure-Requests': '1',
-        #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36',
-        #     'Referer': 'http://xk.urp.seu.edu.cn/jw_service/service/lookCurriculum.action',
-        #     'Content-Type': 'application/x-www-form-urlencoded',
-        #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        #     'Accept-Encoding': 'gzip, deflate',
-        #     'Accept-Language': 'zh-CN,zh;q=0.8'
-        #     }
+        # headers
         self.header = {
             'Host': 'xk.urp.seu.edu.cn',
             'Connection': 'keep-alive',
@@ -49,56 +35,62 @@ class Login(object):
             'Accept-Language': 'zh-CN,zh;q=0.8',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
             }
-        self.request = urllib2.Request(self.url,self.post_data,self.header)
+        self.request = urllib2.Request(self.url, self.post_data, self.header)
 
-        # self.response = self.opener.open(self.request)
+        self.outfile = codecs.open("2016.txt", 'a+', 'utf-8')
+        # self.outfile.write(u'一卡通号\t学号\t姓名\t\t院系\t\t\t专业\n')
+        self.item = []
+
         for j in range(5):
             try:
-                print "The ", j+1, " times try"
+                # print "The ", j+1, " times try"
                 self.response = self.opener.open(self.request)
-                for item in self.cookies:
-                    print item.name,' = ',item.value
+                # for item in self.cookies:
+                #     print item.name, ' = ', item.value
                 break
             except urllib2.URLError, e:
                 print "in the except block"
-                print e.code,e.reason
-                time.sleep(random.uniform(8,15))
-                print "wait for 10 seconds"
+                print e.args, e.reason
+                print "wait for about 10 seconds"
+                time.sleep(random.uniform(8, 15))
                 pass
-        # try:
-        #     self.response = self.opener.open(self.request)
-        #     pass
-        # except urllib2.URLError, e:
-        #     if hasattr(e, "code"):
-        #         print e.code
-        #     if hasattr(e, "reason"):
-        #         print e.reason
-        # except Exception, e:
-        #     print Exception, ":", e
+
+    def find(self, pattern, text):
+        pat = re.compile(pattern, re.U)
+        match = pat.search(text)
+        if match:
+            return match.group(1)
+        else:
+            return False
 
     def get_info(self):
-        page = self.response.read().decode('utf-8')
-        pattern = re.compile(r'<td.*?width="20%".*?align="left">(.*?)</td>', re.S)
+        # build re
+        # pattern = re.compile(r'<td.*?width="20%".*?align="left">(.*?)</td>', re.S)
 
-        if page:
+        page = self.response.read().decode('utf-8')
+        if self.find(ur'(没有找到该学生信息)', page):
+            return "Sorry, Card_No.", self.stuid, " student does not exist"
+        elif page:
             pass
         else:
-            return "No.", self.stuid, " student does not exist"
+            return "no data"
+        card = self.find(ur'一卡通号:(\d+)', page)
+        student_id = self.find(ur'学号:(\w+)', page)
+        name = self.find(ur'姓名:(\w+)', page)
+        college = self.find(ur'院系:\[\w+\](\w+)', page)
+        major = self.find(ur'专业:\[\w+\](\w+)', page)
 
-        print "Result:"
-        items = re.findall(pattern, page)
-        if items:
-            for item in items:
-                print item
+        print card, student_id, name, college, major
 
-            return None
-        else:
-            return "No data"
+        # u'一卡通号\t学号\t姓名\t\t院系\t\t\t专业'
+        self.outfile.write(u'%10s%10s%10s%10s%10s\n' % (card, student_id, name, college, major))
+        self.outfile.close()
 
 if __name__ == '__main__':
-    for i in range(213140001,213143999):
-        semester='16-17-1'
-        log = Login(i,semester)
+    codecs.open("2016.txt", 'w', 'utf-8').write(u'%10s%10s%15s%15s%15s\n' % ('card_id', 'stu_id', 'name', 'college', 'major'))
+    for i in range(213160001,213163968):
+        sem = '16-17-1'
+        log = Login(i, sem)
         log.get_info()
         del log
-        time.sleep(random.uniform(20,25))
+        # time.sleep(random.uniform(20,25))
